f710342aeef6e14257b95b3938beae973cb94c12
Kulbicki Andrey <kubl-ml@mail.ru>
2023-01-17 20:23:15 +0300

    password encryption

--- src/main/java/com/andrew/SpringSecurity/services/RegistrationService.java
+++ src/main/java/com/andrew/SpringSecurity/services/RegistrationService.java
@@ -3,6 +3,7 @@ package com.andrew.SpringSecurity.services;
 import com.andrew.SpringSecurity.models.Person;
 import com.andrew.SpringSecurity.repositories.PeopleRepositories;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -10,14 +11,19 @@ import org.springframework.transaction.annotation.Transactional;
 public class RegistrationService {
 
     private final PeopleRepositories peopleRepositories;
+    private final PasswordEncoder passwordEncoder;
 
     @Autowired
-    public RegistrationService(PeopleRepositories peopleRepositories) {
+    public RegistrationService(PeopleRepositories peopleRepositories, PasswordEncoder passwordEncoder) {
         this.peopleRepositories = peopleRepositories;
+        this.passwordEncoder = passwordEncoder;
     }
 
     @Transactional
     public void register(Person person){
+        String encodedPassword = passwordEncoder.encode(person.getPassword());
+        person.setPassword(encodedPassword);
+
         peopleRepositories.save(person);
     }
 }
