5f0167352afd026077a8a3989b51252bf43c8e35
Kulbicki Andrey <kubl-ml@mail.ru>
2023-01-25 17:37:08 +0300

    access limitation with annotation PreAuthorize

--- src/main/java/com/andrew/SpringSecurity/config/SecurityConfig.java
+++ src/main/java/com/andrew/SpringSecurity/config/SecurityConfig.java
@@ -4,6 +4,7 @@ import com.andrew.SpringSecurity.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.method.configuration.EnableGlobalMethodSecurity;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
@@ -12,6 +13,7 @@ import org.springframework.security.crypto.password.NoOpPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
+@EnableGlobalMethodSecurity(prePostEnabled = true)
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
     private final PersonDetailsService personDetailsService;
@@ -24,7 +26,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http.authorizeRequests()
-                .antMatchers("/admin").hasRole("ADMIN")
+                //.antMatchers("/admin").hasRole("ADMIN")
                 .antMatchers("/auth/login", "/auth/registration", "/error").permitAll()
                 .anyRequest().hasAnyRole("USER", "ADMIN")
                 .and()
--- src/main/java/com/andrew/SpringSecurity/controllers/HelloController.java
+++ src/main/java/com/andrew/SpringSecurity/controllers/HelloController.java
@@ -1,6 +1,8 @@
 package com.andrew.SpringSecurity.controllers;
 
 import com.andrew.SpringSecurity.security.PersonDetails;
+import com.andrew.SpringSecurity.services.AdminService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.context.SecurityContextHolder;
 import org.springframework.stereotype.Controller;
@@ -8,6 +10,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class HelloController {
+
+    private final AdminService adminService;
+
+    @Autowired
+    public HelloController(AdminService adminService) {
+        this.adminService = adminService;
+    }
 
     @GetMapping("/hello")
     public String sayHello(){
@@ -25,6 +34,7 @@ public class HelloController {
 
     @GetMapping("/admin")
     public String adminPage(){
+        adminService.adminStuff();
         return "admin";
     }
 }
